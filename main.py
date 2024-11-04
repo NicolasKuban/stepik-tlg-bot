9620256d49f254ae5c1f3b8a181c97875dceae44
NicolasKuban <nicrad@yandex.ru>
2024-11-05 00:19:21 +0300

    fix: Реализована функция вывода картинки в телеграм-боте

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+
 import requests
 import time
 
 
-from config import API_URL, TOKEN
+from config import API_CATS_URL, API_URL, ERROR_TEXT, TOKEN
 
 
 chat_id: int
+
+
+class CantGetPhoto(Exception):
+    pass
+
+
+def get_cat_photo() -> str:
+    """Функция для получения ссылки на фото котика
+
+    Args:
+
+    Returns:
+        str: возращается строка ссылки на фото котика
+    """
+    cat_response: requests.Response
+    cat_photo_link: str
+    cat_response = requests.get(API_CATS_URL)
+    if cat_response.status_code != 200:
+        raise CantGetPhoto
+    cat_photo_link = cat_response.json()[0]['url']
+    return cat_photo_link
 
 
 def say_hello(text: str) -> None:
@@ -22,11 +44,16 @@ def say_hello(text: str) -> None:
                 print(result['message']['text'])
                 offset = result['update_id']
                 chat_id = result['message']['from']['id']
-                requests.get(
-                    f'{API_URL}{TOKEN}'
-                    f'/sendMessage?chat_id={chat_id}&text={text}'
-                )
-
+                try:
+                    requests.get(
+                        f'{API_URL}{TOKEN}'
+                        f'/sendPhoto?chat_id={chat_id}&photo={get_cat_photo()}'
+                    )
+                except CantGetPhoto:
+                    requests.get(
+                        f'{API_URL}{TOKEN}'
+                        f'/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}'
+                    )
         time.sleep(1)
         counter += 1
 
