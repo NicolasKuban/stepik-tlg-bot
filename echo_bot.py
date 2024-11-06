a41bc753c738e2c960ac59606bf268936777539e
NicolasKuban <nicrad@yandex.ru>
2024-11-06 23:05:50 +0300

    feat: Реализовано добавление команд непосредственно в диспетчер через метод register

--- echo_bot.py
+++ echo_bot.py
@@ -12,13 +12,11 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     print("Получена команда '/start'")
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     print("Получена команда '/help'")
     await message.answer(
@@ -27,12 +25,15 @@ async def process_help_command(message: Message):
     )
 
 
-@dp.message()
 async def send_echo(message: Message):
     print(f"Получено сообщение:{message.text}")
     if message.text:
         await message.reply(text=message.text)
 
+
+dp.message.register(process_start_command, Command(commands=['start'])) 
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
