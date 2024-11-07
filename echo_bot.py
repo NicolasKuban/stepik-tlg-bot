ec90fb9a8b5f3a7a9757630edaa9648512e82be1
NicolasKuban <nicrad@yandex.ru>
2024-11-07 23:01:20 +0300

    feat: Реализованf перессылка поступившего сообщения независимо от типа

--- echo_bot.py
+++ echo_bot.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -12,11 +12,13 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
+@dp.message(Command(commands='start'))
 async def process_start_command(message: Message):
     print("Получена команда '/start'")
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
+@dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     print("Получена команда '/help'")
     await message.answer(
@@ -25,35 +27,16 @@ async def process_help_command(message: Message):
     )
 
 
-async def send_photo_echo(message: Message):
-    if message.photo:
-        await message.answer_photo(message.photo[0].file_id)
-
-
-async def send_audio_echo(message: Message):
-    if message.audio:
-        await message.answer_audio(message.audio.file_id)
-
-
-async def send_sticker_echo(message: Message):
-    if message.sticker:
-        await message.answer_sticker(message.sticker.file_id)
-
-
+@dp.message()
 async def send_echo(message: Message):
     print(f"Получено сообщение:{message.text}")
-    if message.text:
-        await message.reply(text=message.text)
-    else:
-        print(message)
-
-
-dp.message.register(process_start_command, Command(commands=['start']))
-dp.message.register(process_help_command, Command(commands=['help']))
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_audio_echo, F.audio)
-dp.message.register(send_sticker_echo, F.sticker)
-dp.message.register(send_echo)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+            'методом send_copy'
+        )
 
 if __name__ == '__main__':
     dp.run_polling(bot)
